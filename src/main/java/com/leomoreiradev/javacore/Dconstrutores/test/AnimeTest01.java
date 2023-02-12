61686a859a85c768ec6607001a66fc560d694a4a
LEANDRO MOREIRA <leandrodev.oliveira@gmail.com>
2023-02-12 18:19:17 -0300

    Sobrecarga de metodos

---
+++ src/main/java/com/leomoreiradev/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,20 @@
+package com.leomoreiradev.javacore.Dconstrutores.test;
+
+import com.leomoreiradev.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        //usando sobrecarga de metodos
+
+        //Metodo 1
+        anime.init("Akudama Drive", "TV", 12);
+
+        //Metodo 2
+        anime.init("Akudama Drive", "TV", 12, "Ação");
+//        anime.setNome("Akudama Drive");
+//        anime.setTipo("TV");
+//        anime.setEpisodios(12);
+        anime.imprime();
+    }
+}
